36cacc7cb0a456074d2565c89755795d0819ad4d
AranMash <aranmash@yandex.ru>
2017-10-26 22:55:24 +0300

    "maven wrapper "

--- src/test/java/com/max/tema/monolit/monolit/MonolitApplicationTests.java
+++ src/test/java/com/max/tema/monolit/monolit/MonolitApplicationTests.java
@@ -3,11 +3,12 @@ package com.max.tema.monolit.monolit;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = ApplicationRunner.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MonolitApplicationTests {
 
 	@Test
